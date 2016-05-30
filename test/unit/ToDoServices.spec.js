26cc9db6211eb48d0eb70458f10a4b67fe40d34a
Annemarie Kohler <annemarie.k89@gmail.com>
2016-05-30 12:52:31 +0100

    Update ToDoServices.spec.js

--- test/unit/ToDoServices.spec.js
+++ test/unit/ToDoServices.spec.js
@@ -5,14 +5,15 @@ describe('ToDoService', function() {
 
   var toDoData = [{text: "ToDo1", completed: true}, {text: "ToDo2", completed: false}];
 
-  beforeEach(inject(function(_ToDoService_, _ToDoFactory_, $httpBackend) {
+  beforeEach(inject(function(_ToDoService_, $httpBackend) {
     ToDoService = _ToDoService_;
-    ToDoFactory = _ToDoFactory_;
     httpBackend = $httpBackend;
   }));
 
   it('fetches a list of todos', function(){
-    httpBackend.expectGET("http://quiet-beach-24792.herokuapp.com/todos.json").respond(toDoData);
+    httpBackend
+      .expectGET("http://quiet-beach-24792.herokuapp.com/todos.json")
+      .respond(toDoData);
 
     var todo1 = new ToDoFactory("ToDo1", true);
     var todo2 = new ToDoFactory("ToDo2", false);
@@ -20,6 +21,7 @@ describe('ToDoService', function() {
     ToDoService.getAll().then(function(todos) {
       expect(todos).toEqual([todo1, todo2]);
     });
+    
     httpBackend.flush();
   });
 });
